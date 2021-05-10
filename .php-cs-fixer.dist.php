aa9340ab414803a900de1e0dc532d784f90df7e7
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2021-05-10 15:27:45 +0200

    Upgrade PHP CS Fixer to v3

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,7 +14,8 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/src')
     ->in(__DIR__.'/tests');
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+return $config
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
