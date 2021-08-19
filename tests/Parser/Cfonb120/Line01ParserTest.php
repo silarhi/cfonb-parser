05cce39672b6885de0a2047fcb357e79a6e88036
gdecorbiac <geoffroydecorbiac@gmail.com>
2021-08-19 10:37:49 +0200

    Allow first reserved characters of line 01 (position 8 to 11) to be not empty (#21)
    * Allow first reserved characters of line 01 (position 8 to 11) to be not empty
    
    * Add test case for not empty unused_1 regex part
    
    Co-authored-by: Geoffroy de Corbiac <gdc@groupearmonia.com>

--- tests/Parser/Cfonb120/Line01ParserTest.php
+++ tests/Parser/Cfonb120/Line01ParserTest.php
@@ -30,9 +30,22 @@ class Line01ParserTest extends TestCase
     }
 
     /** @return void */
-    public function testOk()
+    public function testOkEmptyUnused1()
     {
         $balance = $this->parse('0110278    02204EUR2 00012345603  060420                                                  0000000166956E060420070420    ');
+
+        self::assertSame('2020-04-06', $balance->getDate()->format('Y-m-d'));
+        self::assertSame('02204', $balance->getDeskCode());
+        self::assertSame('EUR', $balance->getCurrencyCode());
+        self::assertSame('10278', $balance->getBankCode());
+        self::assertSame('00012345603', $balance->getAccountNumber());
+        self::assertSame(16695.65, $balance->getAmount());
+    }
+
+    /** @return void */
+    public function testOkNotEmptyUnused1()
+    {
+        $balance = $this->parse('0110278RLV 02204EUR2 00012345603  060420                                                  0000000166956E060420070420    ');
 
         self::assertSame('2020-04-06', $balance->getDate()->format('Y-m-d'));
         self::assertSame('02204', $balance->getDeskCode());
